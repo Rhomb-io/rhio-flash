60a024677053f441acf5ab77f51ee4584e30000e
jose1698 <jmarti@rhomb.io>
2020-11-16 14:07:15 +0100

    Basic example

---
+++ examples/getting_started.cpp
@@ -0,0 +1,40 @@
+#define DEBUG Serial
+#include "rhio-flash.h"
+
+RhioFlash FLASH;
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+
+  DEBUG.begin(115200);
+  while (!DEBUG)
+    ;
+
+  DEBUG.println("Set");
+  FLASH.beginMemory();
+  uint32_t jedecid = FLASH.readManufacterAndDeviceID();
+  uint8_t jedecid1, jedecid2, jedecid3, jedecid4;
+  jedecid1 = jedecid >> 24;
+  jedecid2 = jedecid >> 16;
+  jedecid3 = jedecid >> 8;
+  jedecid4 = jedecid;
+  DEBUG.print("Manufacter ID: ");
+  DEBUG.println(jedecid1, HEX);
+  DEBUG.print("Device ID (Part1): ");
+  DEBUG.println(jedecid2, HEX);
+  DEBUG.print("Device ID (Part2): ");
+  DEBUG.println(jedecid3, HEX);
+  DEBUG.print("Extended: ");
+  DEBUG.println(jedecid4, HEX);
+  FLASH.chipErase();
+  FLASH.writeEnable();
+
+  FLASH.writeByte(0xE3, 0);
+  DEBUG.print("Reading address 0: ");  // 11100011
+  DEBUG.println(FLASH.readByte(0), BIN);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
