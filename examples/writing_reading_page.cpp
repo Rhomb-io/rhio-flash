c333c9e16c8a8f67246b5161ca8b92258cd71b4c
jose1698 <jmarti@rhomb.io>
2020-11-26 09:01:07 +0100

    English comments, eliminated writeEnable

--- examples/writing_reading_page.cpp
+++ examples/writing_reading_page.cpp
@@ -31,46 +31,40 @@ void setup() {
 
   FLASH.setBlockProtection(0);
   DEBUG.println("");
-  DEBUG.println("INFORMACION READING STATUS: ");
-  DEBUG.print("Bit de protección: ");
+  DEBUG.println("READING STATUS: ");
+  DEBUG.print("Protection bit: ");
   DEBUG.println(FLASH.getBlockProtection());
-  DEBUG.print("Bit de bloqueo protección: ");
+  DEBUG.print("Block protection bit: ");
   DEBUG.println(FLASH.getBlockProtectionLocked());
 
-  // FLASH.writeEnable(); Si lo pongo se bloquea (para page no hace falta)
   FLASH.pageErase(0);
 
   uint8_t array[8] = {};
   uint8_t array1[8] = {};
   uint8_t variable = 0;
 
-  FLASH.writeEnable();
-
-  DEBUG.print("Bit escritura habilitado: ");
+  DEBUG.print("Write enable bit: ");
   DEBUG.println(FLASH.getWriteEnable());
   DEBUG.println("");
-  DEBUG.println("A continuación se va a escribir un array de 8 bytes.");
+  DEBUG.println("Writing array of 8 bytes.");
   DEBUG.println("");
 
   for (int k = 0; k < 8; k++) {
     variable = variable + 2 * 2;  // 4, 8, 12, 16 20, 24...
     array[k] = {variable};
-    Serial.println("Generando escritura");
+    Serial.println("Writing generated");
     Serial.println(array[k]);
   }
-  FLASH.writePage(array, 0, 8);
+  FLASH.write(array, 0, 8);
 
   DEBUG.println("");
-  DEBUG.println("Lectura de bytes");
+  DEBUG.println("Reading bytes");
 
-  FLASH.readArray(array1, 0, 8);
+  FLASH.read(array1, 0, 8);
 
   for (int i = 0; i < 8; i++) {
     DEBUG.println(array1[i]);
   }
-  /*for (int i = 0; i < 8; i++) {
-    DEBUG.println(FLASH.readByte(i), DEC);
-  }*/
 }
 
 void loop() {
